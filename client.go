31c8c7eff2e08f85b0e7e49c2c6350db85e4fb67
Tristan Wietsma <tristan.wietsma@gmail.com>
2013-09-28 23:01:40 -0500

    added client-side functionality

--- client.go
+++ client.go
@@ -23,23 +23,52 @@ import (
 
 type ServerConnection struct {
 	conn net.Conn
+	feed chan string
 }
 
-func (sc *ServerConnection) Send(m *Message)  {
-	// to do
+func (sc *ServerConnection) transmit(m *Message) error {
+	b := m.Bytes()
+	n, err := (*sc).conn.Write(b)
+	switch (*m).cmd {
+	case SET, PUB, DEL:
+		return err
+	case GET, SUB:
+
+		buf := make([]byte, 1024)
+
+WAIT_FOR_SERVER:
+		_, err := (*sc).conn.Read(buf)
+		if err != nil {
+			return err
+		}
+
+		end := bytes.IndexByte(buf, EOM)
+		if end < 0 {
+			err := ProtocolError{"Message is missing EOM byte.")
+			return err
+		}
+
+		payload := string(buf[:end])
+		(*sc).feed<-payload
+		if (*m).cmd == SUB {
+			goto WAIT_FOR_SERVER
+		}
+		return nil
+	}
 }
 
-func (sc *ServerConnection) Close() {
-	// to do
+func (sc *ServerConnection) Close() error {
+	err := (*sc).Close()
+	return err
 }
 
-func NewServerConnection(address string, port uint) (ServerConnection, error) {
+func NewServerConnection(address string, port uint) (*ServerConnection, error) {
 	fullAddress := address + ":" + strconv.FormatUint(uint64(port), 10)
 	conn, err := net.Dial("tcp", fullAddress)
 	sc = ServerConnection{}
 	if err == nil {
 		sc.conn = conn
 	}
-	return sc, err
+	return &sc, err
 }
 
